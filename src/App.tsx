326952ec5b6a7624a71842214f101fcaaf7bebad
Mikita <anikitosiknik@gmail.com>
2021-09-03 21:12:43 +0200

    WAPP-1: remove unnecessary components, fix formating

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-import ReduxTest from './ReduTest';
-import StyledTest from './StyledTest';
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import styled from "styled-components";
 
+const Main = styled.main`
+  height: 100vh;
+`;
 
 function App() {
-
-
-
   return (
     <Provider store={store}>
-      
-    <div className="App">
-      <header className="App-header">
-        <StyledTest/>
-        <ReduxTest/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
+      <Main></Main>
     </Provider>
   );
 }
 
 export default App;
-
-
-
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit"
-import counterSlice from "./counterSlice"
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-  reducer: {
-    counter: counterSlice
-  },
-})
+  reducer: {},
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
